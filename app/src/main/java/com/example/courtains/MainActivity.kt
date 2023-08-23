658d93556c7b001e705c1c12ddf670b328dc9841
Adair Duarte <ada_dp10@hotmail.com>
2023-08-22 23:42:10 -0600

    Ready
    Ya todo funciona correctamente

--- app/src/main/java/com/example/courtains/MainActivity.kt
+++ app/src/main/java/com/example/courtains/MainActivity.kt
@@ -8,39 +8,48 @@ import com.example.courtains.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    var tiempoFinal = ""
+    var tiempoFinal = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //Se inicializa el binding
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
 
         binding.btnIniciar.setOnClickListener {
             Cambio()
         }
 
-        binding.btn1.setOnClickListener {AgregarTiempo(binding.btn1.text.toString())}
-        binding.btn2.setOnClickListener {AgregarTiempo(binding.btn2.text.toString())}
-        binding.btn3.setOnClickListener {AgregarTiempo(binding.btn3.text.toString())}
-        binding.btn4.setOnClickListener {AgregarTiempo(binding.btn4.text.toString())}
-        binding.btn5.setOnClickListener {AgregarTiempo(binding.btn5.text.toString())}
-        binding.btn6.setOnClickListener {AgregarTiempo(binding.btn6.text.toString())}
-        binding.btn7.setOnClickListener {AgregarTiempo(binding.btn7.text.toString())}
-        binding.btn8.setOnClickListener {AgregarTiempo(binding.btn8.text.toString())}
-        binding.btn9.setOnClickListener {AgregarTiempo(binding.btn9.text.toString())}
-        binding.btn0.setOnClickListener {AgregarTiempo(binding.btn0.text.toString())}
+        binding.btnBorrar.setOnClickListener {
+            binding.txtTime.text = "0"
+            tiempoFinal = 0
+        }
+
+        //Se agega los numeeros al metodo segun el usuario haga click
+        binding.btn1.setOnClickListener {AgregarTiempo(binding.btn1.text.toString().toInt())}
+        binding.btn2.setOnClickListener {AgregarTiempo(binding.btn2.text.toString().toInt())}
+        binding.btn3.setOnClickListener {AgregarTiempo(binding.btn3.text.toString().toInt())}
+        binding.btn4.setOnClickListener {AgregarTiempo(binding.btn4.text.toString().toInt())}
+        binding.btn5.setOnClickListener {AgregarTiempo(binding.btn5.text.toString().toInt())}
+        binding.btn6.setOnClickListener {AgregarTiempo(binding.btn6.text.toString().toInt())}
+        binding.btn7.setOnClickListener {AgregarTiempo(binding.btn7.text.toString().toInt())}
+        binding.btn8.setOnClickListener {AgregarTiempo(binding.btn8.text.toString().toInt())}
+        binding.btn9.setOnClickListener {AgregarTiempo(binding.btn9.text.toString().toInt())}
+        binding.btn0.setOnClickListener {AgregarTiempo(binding.btn0.text.toString().toInt())}
     }
 
-
+    //Se manda llamar al intent para hacer el cambio con el tiempo agregado
     private fun Cambio(){
         val intent = Intent(this, Progress::class.java)
         intent.putExtra("Time", binding.txtTime.text.toString())
         startActivity(intent)
     }
 
-    private fun AgregarTiempo(tiempo: String) {
-        tiempoFinal += tiempo
-        binding.txtTime.text = tiempoFinal
+    //Se agrega la variable tiempo al tiempoFinal
+    private fun AgregarTiempo(tiempo: Int) {
+        tiempoFinal = tiempoFinal * 10 + tiempo
+        binding.txtTime.text = tiempoFinal.toString()
     }
 
 
--- app/src/main/java/com/example/courtains/Progress.kt
+++ app/src/main/java/com/example/courtains/Progress.kt
@@ -2,9 +2,11 @@ package com.example.courtains
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.courtains.databinding.ActivityProgressBinding
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
@@ -12,19 +14,25 @@ class Progress : AppCompatActivity() {
 
     private lateinit var binding: ActivityProgressBinding
     var salir = true
+    //Se inicializa la variable Job para el control de los courtains
+    var contarJob: Job? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //Se iniciliaza el binding
         binding = ActivityProgressBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        //Se obtine la variable Time del layout anterior
         var time = intent.getStringExtra("Time")
+        //Verifica si no es null la entrada
         if (time != null) {
+            //Se pone asigna el maximo en el progresBar acorde al Time
             binding.progressBar.max = time.toInt()
+            //Se asigna el valor dentro del progressBar
             binding.etqNumero.text = time
-            GlobalScope.launch(Dispatchers.Main) {
-                contar()
-            }
+            //Se manda llamar el conteo
+            iniciarConteo()
         }
 
 
@@ -36,24 +44,55 @@ class Progress : AppCompatActivity() {
             }else{
                 salir=true
                 binding.btnIniciar.text="Detener"
-                GlobalScope.launch(Dispatchers.Main) {
-                    contar()
-                }
+                iniciarConteo()
             }
 
         }
+
+        binding.btnBorrar.setOnClickListener{
+            onBackPressed()
+        }
+
+        binding.btnAgregar.setOnClickListener {
+            Agregar()
+            salir = true
+            iniciarConteo()
+        }
     }
 
-    //Suspend es la manera en la que s epueden utiloizar las courtains
+    //es la manera de asignar el Coroutine
+    private fun iniciarConteo() {
+        contarJob?.cancel()  // Cancela la corrutina anterior si existe
+        //Inicializa el Coroutine
+        contarJob = GlobalScope.launch(Dispatchers.Main) {
+            contar()
+        }
+    }
+    //Se inicializa el conteo
     suspend fun contar(){
+        //Obtiene el numero de conteo
         var num = binding.etqNumero.text.toString().toInt()
         while (salir && num >= 0){
             delay(1000)
-            //Se recibe el valor
             binding.etqNumero.text = num.toString()
             binding.progressBar.setProgress(num)
             num --
-        }
 
+            // Muestra un Toast cuando el temporizador llegue a 0
+            if (num <= 0) {
+                Toast.makeText(this, "Tu tiempo ha terminado.", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    //Se agrega 60 segundos
+    fun Agregar(){
+        //Asigna el valor del temporizador
+        var num = binding.etqNumero.text.toString().toInt()
+        //Le asigna 60 segundos mas y lo que tenia
+        num += 60
+        salir = false
+        binding.etqNumero.text = num.toString()
+        binding.progressBar.max = num
     }
 }
